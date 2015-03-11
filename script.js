137d780bf903f2916654409772b17dd2b66dda1f
hakon0601 <hakongimse@gmail.com>
2015-03-11 19:29:56 +0100

    working graph

--- script.js
+++ script.js
@@ -2,37 +2,47 @@
  * Created by sondredyvik on 09.03.15.
  */
 var chart1;
-var tickCounter= 0;
+var allText;
+
+var urls = ["http://hoffson.me/capitalist/stocks/aapl_out.txt", "http://hoffson.me/capitalist/stocks/bud_out.txt", "http://hoffson.me/capitalist/stocks/goog_out.txt", "http://hoffson.me/capitalist/stocks/gs_out.txt", "http://hoffson.me/capitalist/stocks/mcd_out.txt", "http://hoffson.me/capitalist/stocks/tsla_out.txt"];
+
+var all_stocks = [{}, {}, {}, {}, {}, {}];
+var days = [];
+
+x_ticks = [[], [], [], [], [], []];
+var tickCounter = 0;
 var running= false;
+
 function intervalTrigger(){
     return window.setInterval(updateGraph,500);
 }
 
-
 var interval;
 function updateGraph() {
+    console.log("Tickcount: " + tickCounter);
     if (tickCounter==0){
+        days = [];
 
-
-        chart1.series[0].setData([]);
-        chart1.series[1].setData([]);
-        chart1.series[2].setData([]);
+        for (var j = 0 ; j < all_stocks.length ; j++) {
+            days[j] = selectDay(all_stocks[j])
+            x_ticks[j] = [];
+            chart1.series[j].setData([]);
+        }
 
     }
-    if(tickCounter <= 5){
-
-        chart1.series[0].addPoint(tickCounter*5);
-        chart1.series[1].addPoint(tickCounter*11);
-        chart1.series[2].addPoint(tickCounter*12);
-
-        chart1.redraw();
-        console.log('fucker');
-        tickCounter +=1;
-        }
-    else{
+    if (tickCounter > 30) {
         window.clearInterval(interval);
         running = false;
         tickCounter = 0;
+    } else {
+        for (var k = 0; k < all_stocks.length; k++) {
+            console.log()
+            x_ticks[k][x_ticks[k].length] = days[k][tickCounter][1];
+            chart1.series[k].addPoint(parseInt(x_ticks[k][tickCounter]));
+        }
+
+        chart1.redraw();
+        tickCounter += 1;
     }
 
 }
@@ -40,7 +50,6 @@ function startSimulation(){
     if (running == false){
         interval = intervalTrigger();
         running = true;
-
     }
 }
 
@@ -64,6 +73,7 @@ $(document).ready(function() {
                 text: 'Value'
             },
             ordinal: false,
+            min:0,
             max:100
 
         },
@@ -71,21 +81,67 @@ $(document).ready(function() {
             name: 'Apple',
             data: [null]
         }, {
-            name: 'Tesla',
+            name: 'Budweiser',
             data: [null]
         }, {
             name: 'Google',
             data: [null]
+        }, {
+            name: 'Goldman Sachs',
+            data: [null]
+        }, {
+            name: 'McDonalds',
+            data: [null]
+        }, {
+            name: 'Tesla',
+            data: [null]
         }]
     });
 
-
-
-
-
-
-
-
-
+    for (var i = 0 ; i < all_stocks.length ; i++) {
+        readTextFile(urls[i], i);
+    }
 });
+
+function readTextFile(file, index)
+{
+    var array = all_stocks[index];
+    var rawFile = new XMLHttpRequest();
+    rawFile.open("GET", file, true);
+    rawFile.onreadystatechange = function ()
+    {
+        if(rawFile.readyState === 4)
+        {
+            if(rawFile.status === 200 || rawFile.status == 0)
+            {
+                allText = rawFile.responseText.split('\n');
+                for (var i = 0 ; i < allText.length ; i++) {
+                    var a = allText[i].split(', ');
+                    var values = [a[1], a[2]];
+                    var date = String(a[0]);
+
+                    if (date in array == false) {
+                        array[date] = [];
+                    }
+                    console.log(date);
+                    array[date][array[date].length] = values;
+                }
+                all_stocks[index] = array;
+            }
+        }
+    }
+    rawFile.send(null);
+    rawFile.onreadystatech
+}
+
+function selectDay(array){
+    var randKey;
+    while (randKey == null || randKey.length != 10) {
+        var keys = Object.keys(array);
+        var randKey = keys[Math.floor(keys.length * Math.random())]
+        console.log(randKey);
+        console.log(randKey.length);
+    }
+    return array[randKey];
+}
 
